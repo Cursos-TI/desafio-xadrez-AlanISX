a283a31e42d9c1496dee7663fd2f5f1989d992b6
AlanISX <alanmagalhaes980@gmail.com>
2025-03-23 13:36:33 +0000

    Desafio mestre

--- xadrez.c
+++ xadrez.c
@@ -1,51 +1,73 @@
 #include <stdio.h>
 
+// Função recursiva para os movimentos da Torre (apenas para cima)
+void movmen(int movt)
+{
+    if (movt > 0)
+    {
+        printf("Cima\n");
+        movmen(movt - 1);
+    }
+}
+
+// Função que imprime os movimentos do Bispo
+void mov(int movb)
+{
+    // Loop externo para movimentação diagonal (Cima)
+    for (int bisp = 0; bisp < movb; bisp++)
+    {
+        printf("Cima ");
+
+        // Loop interno para apenas um movimento horizontal (Direita)
+        printf("Direita\n");
+    }
+}
+
+// Função recursiva para os movimentos da Rainha (apenas para a esquerda)
+void movimento(int movrn)
+{
+    if (movrn > 0)
+    {
+        printf("Esquerda\n");
+        movimento(movrn - 1); // Chama recursivamente
+    }
+}
+
+void movme(int movc)
+{
+    for (int i = 0; i < movc; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+}
+
 int main()
 {
 
     /*Utilizei apenas a letra inicial para cada peça sendo assim
-    R = Rei, Rn = Rainha, B = Bispo, C = Cavalo, T = Torre e P = Peão */
+    R = Rei, Rn = Rainha, B = Bispo, C = Cavalo e T = Torre*/
 
-    int t, b = 0, rn = 0, c = 0;
+    int t = 5, b = 5, rn = 8, c = 1;
 
-    // For utilizado para limitar movimento da torre à 5 casas
-    printf("Movimento Torre: \n");
-    for (t = 0; t < 5; t++)
-    {
-        printf("Cima\n");
-    }
+    // Exibindo os movimentos do Torre
+    printf("\nMovimento Torre: \n");
+    movmen(t);
 
-    // Limita o movimento à 8 casas
+    // Exibindo os movimentos do Rainha
     printf("\nMovimento Rainha: \n");
-    do
-    {
-        printf("Esquerda\n");
-        rn++;
-    } while (rn < 8);
+    movimento(rn);
 
-    // Limita o movimento à 5 casas
+    // Exibindo os movimentos do Bispo
     printf("\nMovimento Bispo: \n");
-    while (b <= 5)
-    {
-        printf("Cima Direita\n");
-        b++;
-    }
+    mov(b);
 
-    // Declarando movimentação do cavalo, limitando-se à duas casas para cima e uma para a direita
-    for (int c = 0; c < 2; c++)
-    {
-        printf("\nMovimento Cavalo: \n");
-
-        do
-        {
-            printf("Cima\n");
-            c++;
-
-        } while (c < 2);
-        {
-            printf("Direita\n");
-        }
-    }
+    // Exibindo os movimentos do Cavalo
+    printf("\nMovimento Cavalo: \n");
+    movme(c);
 
     return 0;
 }
